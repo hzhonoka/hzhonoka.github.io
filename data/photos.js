af5baf2d4147c81e75c400eeb6ef57a1320a53c4
unknown <251250047@smail.nju.edu.cnemail 251250047@smail.nju.edu.cn>
2026-07-29 23:32:26 +0800

    7/29

--- data/photos.js
+++ data/photos.js
@@ -3,5 +3,6 @@
  * 然后在下面最上方加一条记录，src 填 "assets/photos/文件名.jpg"。
  * 也支持直接填网络图片地址。注意逗号别漏掉！ */
 window.PHOTOS = [
+  { src: "assets/photos/星空20260729.jpg", caption: "夏季大三角" },
   { src: "assets/photos/妖梦.jpg", caption: "示例照片（妖梦）(仅测试 侵权联系删除）" },
 ];
